7de674d65ea62041de89e5e480549b57b0c0eba4
Cazimir Roman <cazimir.roman@gmail.com>
2018-11-14 12:30:15 +0100

    added some more tests

--- app/src/test/java/cazimir/com/magazoo/MapPresenterTest.java
+++ app/src/test/java/cazimir/com/magazoo/MapPresenterTest.java
@@ -215,6 +215,7 @@ public class MapPresenterTest {
     @Test
     public void shouldShowTheAddShopDialogIfAllowedToAdd() {
 
+
         String userId = "random user id";
         ArrayList<Shop> shops = new ArrayList<>();
 
